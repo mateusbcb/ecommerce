ac1a236f0fb6d672f617f00b7f3837f21dab240a
Mateus Beloso Caniçais Brandt <38787016+mateusbcb@users.noreply.github.com>
2019-09-10 20:23:09 -0300

    criamos a class pages

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\pages;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
-	
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
+	$page = new Pages();
+	$page->setTPL("index");
 
 });
 
