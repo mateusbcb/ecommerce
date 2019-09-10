ecd67ed73abe458cf07945fed11daccd2acc85cb
Mateus Beloso Caniçais Brandt <38787016+mateusbcb@users.noreply.github.com>
2019-09-10 19:02:11 -0300

    autoload hcode classes

---
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace Hcode\DB;
+
+class Sql {
+
+	const HOSTNAME = "127.0.0.1";
+	const USERNAME = "root";
+	const PASSWORD = "";
+	const DBNAME = "db_ecommerce";
+	const PORT = "3308";
+
+	private $conn;
+
+	public function __construct()
+	{
+
+		$this->conn = new \PDO("mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME.";port=".Sql::PORT, Sql::USERNAME, Sql::PASSWORD);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
