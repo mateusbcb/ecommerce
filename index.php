153409cc4372ad4918f99f2a8a9dfecd4d372b02
Mateus Beloso Caniçais Brandt <38787016+mateusbcb@users.noreply.github.com>
2019-09-17 18:21:07 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,31 +1,59 @@
 <?php 
-
-require_once("vendor/autoload.php");
-
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
+	session_start();
+	
+	require_once("vendor/autoload.php");
 
 
-$app = new Slim();
+	use \Slim\Slim;
+	use \Hcode\Page;
+	use \Hcode\PageAdmin;
+	use \Hcode\Model\User;
+	
+	$app = new Slim();
 
-$app->config('debug', true);
+	$app->get('/', function() {
+		
+		$page = new Page();
+		$page->setTPL("index");
 
-$app->get('/', function() {
+	});
+	
+	$app->get('/admin', function() {
     
-	$page = new Page();
-	$page->setTPL("index");
+		User::verifyLogin();
+		
+		$page = new PageAdmin();
+		$page->setTpl("index");
+		
+	});
+	
+	$app->get('/admin/login', function(){
+			
+		$page = new PageAdmin(
+			"header"=>false,
+			"footer"=>false
+		);
+		$page->setTPL("login");
+		
+	});
+	
+	$app->post('/admi/login', function(){
+		
+		User::login($_POST['login'], $_POST['Password']);
+		
+		header("Location: /admin");
+		exit;
+		
+	});
+	
+	$app->get('/admin/logout', function(){
+		
+		User::logout();
+		
+		header("Location: /admin/login");
+		
+	});
 
-});
-
-
-$app->get('/admin', function() {
-    
-	User::verifyLogin();
-	$page = new PageAdmin();
-	$page->setTpl("index");
-});
-
-$app->run();
+	$app->run();
 
  ?>
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,67 @@
+<?PHP
+	
+	namespace Hcode\Model;
+	
+	use \Hcode\DB\Sql;
+	use \Hcode\Model;
+	
+	class User extends Model{
+		
+		const SESSION = "user";
+		
+		public static function login($login, $Password){
+			
+			$sql = new Sql();
+			
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+				":LOGIN"=>$login
+			));
+			
+			if(count($results) == 0){
+				
+				throw new \Exception("Usuario inexistente ou senha inválida", 1);
+			}
+			
+			$data = $results[0];
+			
+			if (Password_verify($password, $data["despassword"]) == true){
+				
+				$user = new User();
+				
+				$user->setData($data);
+				
+				$_SESSION[User::SESSION] = $user->getValues();
+				
+				return $user;
+				
+			}else{
+				throw new \Exception("Usuario inexistente ou senha inválida", 1);
+			}
+			
+		}
+		
+		public function verifyLogin($inadmin = true){
+			
+			if(
+				!isset($_SESSION[User::SESSION]) ||
+				!$_SESSION[User::SESSION] ||
+				!(int)$_SESSION[User::SESSION]["iduser"] > 0 ||
+				(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+			){
+				
+				header("Location: /admin/login");
+				exit;
+				
+			}
+			
+		}
+		
+		public function logout(){
+			
+			$_SESSION[User::SESSION] = NULL;
+			
+		}
+		
+	}
+	
+?>
