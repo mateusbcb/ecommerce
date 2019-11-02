364a91cd0695c13803cfd6d8f18251edd97da845
Mateus Beloso Caniçais Brandt <38787016+mateusbcb@users.noreply.github.com>
2019-11-02 11:45:22 -0300

    Carrinha de compras

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 	use \Hcode\Page;
 	use \Hcode\Model\Product;
 	use \Hcode\Model\Category;
+	use \Hcode\Model\Cart;
 	
 	$app->get('/', function() {
 		
@@ -55,6 +56,15 @@
 			'product'=>$product->getValues(),
 			'categories'=$product->getCategories()
 		]);
+	});
+	
+	$app->get('/cart', function(){
+		
+		$cart = Cart::getFromSession();
+		
+		$page = new Page();
+		
+		$page->setTpl("cart");
 	});
 	
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,42 @@
 		const SESSION = "user";
 		const SECRET = "HcodePHP7_Secret";
 		
+		public static function getFromSession(){
+			
+			$user = new User();
+			
+			if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0){
+				
+				$user->setData($_SESSION[User::SESSION]);
+			}
+			
+			return $user;
+		}
+		
+		public static function checklogin($inadmin = true){
+			
+			if(
+				!isset($_SESSION[User::SESSION]) ||
+				!$_SESSION[User::SESSION] ||
+				!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			){
+				//Não esta logado
+				return false;
+			}else{
+				
+				if($inadmin === true && (bool)$_SESSION[User::SESSION]["inadmin"] === true){
+					
+					return true;
+				}else if($inadmin === false){
+					
+					return true;
+				}else{
+					
+					return false;
+				}
+			}
+		}
+		
 		public static function login($login, $Password){
 			
 			$sql = new Sql();
@@ -44,16 +80,10 @@
 		
 		public static function verifyLogin($inadmin = true){
 			
-			if(
-				!isset($_SESSION[User::SESSION]) ||
-				!$_SESSION[User::SESSION] ||
-				!(int)$_SESSION[User::SESSION]["iduser"] > 0 ||
-				(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-			){
+			if(User::checklogin($inadmin)){
 				
 				header("Location: /admin/login");
 				exit;
-				
 			}
 			
 		}
